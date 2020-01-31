42364bf0403e6a97334888eebf2f2ab6d5bf4a23
Maciej Baranowski <maciekbaranowski@onet.pl>
2020-01-31 11:38:02 +0100

    Basic naive solution - take as many pizzas from beginning as possible

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import utils
 
 def solution(input : utils.InputHandler, output : utils.OutputHandler):
-    output.writeline(input.loadMultipleElementLine())
-    #algorithm goes here
+    [maxNumOfSlices, numOfPizzas] = input.loadMultipleElementLine()
+    numOfSlicesInPizza = input.loadMultipleElementLine()
+    selectedPizzas = []
+
+    #Algorithm: just naively take pizzas from smallest one until max is reached
+    slicesCounter = 0
+    pizzaCounter = 0
+    while pizzaCounter < numOfPizzas:
+        if slicesCounter + numOfSlicesInPizza[pizzaCounter] <= maxNumOfSlices:
+            slicesCounter += numOfSlicesInPizza[pizzaCounter]
+            pizzaCounter += 1
+        else:
+            break
+
+    selectedPizzas = list(range(0,pizzaCounter))
+
+    output.writeline(len(selectedPizzas))
+    output.writeline(selectedPizzas)
 
 utils.processAll(solution)
